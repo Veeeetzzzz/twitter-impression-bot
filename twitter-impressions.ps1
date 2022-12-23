f1359e6ce947d425315560953003dc93aa2e4653
Veeeetzzzz <40268197+Veeeetzzzz@users.noreply.github.com>
2022-12-23 21:37:27 +0000

    Create twitter-impressions.ps1

---
+++ twitter-impressions.ps1
@@ -0,0 +1,37 @@
+# Prompt the user for the target website and the location of the text file
+$targetWebsite = Read-Host "Enter the target website (e.g. https://www.example.com): "
+$proxyFile = Read-Host "Enter the location of the text file containing the list of proxies: "
+
+# Read the list of proxies from the text file
+$proxies = Get-Content $proxyFile
+
+# Set the initial visit count to 0
+$visitCount = 0
+
+# Set an infinite loop to send requests to the website every 5 seconds
+while ($true) {
+  # Select a proxy from the list at random
+  $proxy = $proxies | Get-Random
+
+  # Create a WebRequest object using the selected proxy
+  $request = [System.Net.WebRequest]::Create($targetWebsite)
+  $request.Proxy = New-Object System.Net.WebProxy($proxy)
+
+  # Send the request and get the response
+  $response = $request.GetResponse()
+
+  # Increment the visit count if the request was successful (status code 200)
+  if ($response.StatusCode -eq 200) {
+    $visitCount++
+    Write-Host "OK - Visit Count $visitCount"
+  }
+  else {
+    Write-Host "Failed - Visit Count $visitCount"
+  }
+
+  # Close the response stream
+  $response.Close()
+
+  # Wait for 5 seconds before sending the next request
+  Start-Sleep -Seconds 5
+}
